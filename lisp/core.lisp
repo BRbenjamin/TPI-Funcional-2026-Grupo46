663c9cdc8de491915c22c1ab9e92019deec81487
Angel Francisco Pindo <pindofrancisco9@gmail.com>
2026-06-10 18:32:39 -0300

    Update core.lisp

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -25,3 +25,39 @@
 		((and (>= (mod tiempo_actual 216) 96) (<= (mod tiempo_actual 216) 215)) 'verde)
 	)
 )
+
+; 3er requerimiento: Auditoria
+(ql:quickload "local-time")
+
+ (defun Auditoria()
+
+	(let ((id) (clave) (tiempo))
+		(print "Ingrese su ID: ")
+			(setq id(read))
+		(print "Ingrese la clave: ")
+			(setq clave(read))
+
+			(if (equal clave 1234)
+
+				(progn
+					(print "Ingrese tiempo Unix para saber el color anterior y el siguiente: ")
+						(setq tiempo(read))
+					(list (local-time:format-timestring nil
+					 								(local-time:unix-to-timestamp tiempo) :format '(:year "-" :month "-" :day " " :hour ":" :min ":" :sec))
+																    (calcular-tiempo  tiempo)))
+			)
+	)
+)
+
+(defun calcular-tiempo (tiempo)
+
+	(let ((ciclo-semaforo 216))
+		(cond
+			((and (>= (mod tiempo ciclo-semaforo) 0) (<= (mod tiempo ciclo-semaforo) 89))  "La luz ha cambiado de Amarillo a Rojo")
+
+			((and (>= (mod tiempo ciclo-semaforo) 90) (<= (mod tiempo ciclo-semaforo) 209))  "La luz ha cambiado de Rojo a Verde")	
+			
+			((and (>= (mod tiempo ciclo-semaforo) 210) (<= (mod tiempo ciclo-semaforo) 215))  "La luz ha cambiado de Verde a Amarillo")
+		)
+	)
+)
