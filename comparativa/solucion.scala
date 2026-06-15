a236a8cc47c921cd5fa72fa30e322f51dbc985d6
BenjaminRomero <benjaminromero2404@gmail.com>
2026-06-15 19:40:39 -0300

    Segunda parte de solucion.scala

--- comparativa/solucion.scala
+++ comparativa/solucion.scala
@@ -26,4 +26,41 @@ object SistemaSemaforo {
         Left("Datos no Válidos")
     }
   }
+
+  /**
+    2do Requerimiento: Temporizador Automático
+    Funcion: temporizador
+    Naturaleza: Pura
+    Estrategia: Utilizacion de residuo matemático % y Pattern Matching con Guards
+    Impacto: No Destructiva
+   */
+  def temporizador(tiempoUnix: Long): Color = {
+    val tiempoCiclo = 225 
+    val resto = Math.floorMod(tiempoUnix, tiempoCiclo)
+
+    resto match {
+      case r if r >= 0   && r <= 89  => Color.Rojo
+      case r if r >= 90  && r <= 92  => Color.IntermitenteAmarillo
+      case r if r >= 93  && r <= 212 => Color.Verde
+      case r if r >= 213 && r <= 215 => Color.IntermitenteVerde
+      case r if r >= 216 && r <= 221 => Color.Amarillo
+      case r if r >= 222 && r <= 224 => Color.IntermitenteAmarillo
+      case _                         => Color.Rojo
+    }
+  }
+}
+
+import scala.io.StdIn.readLine
+
+@main def auditoriaInteractiva() = {
+  print("Ingrese tiempo Unix para saber el color: ")
+  val Color = readLine()
+  
+  // Convertimos el texto ingresado a número largo (Long)
+  val tiempo = Color.toLong 
+  
+  val resultado = SistemaSemaforo.temporizador(tiempo)
+  
+  // Imprime únicamente la respuesta pura
+  println(s"Color: $resultado")
 }
